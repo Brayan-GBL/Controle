5281a2f3a627d4189f3b858d41ed8574e46c5a92
Brayan-GBL <brayanquiterio2000@gmail.com>
2025-02-13 14:27:50 -0300

    Update Posigraf_2.py
    Alteração de descrição caso o saldo não seja encontrado

--- Posigraf_2.py
+++ Posigraf_2.py
@@ -24,7 +24,7 @@ def processar_comparacao(pedidos_file, estoque_file):
     resultado_df = pd.merge(pedidos_consolidado, estoque_df[["Item", "Estoque"]], on="Item", how="left")
     
     # Preencher valores ausentes com "Não encontrado saldo"
-    resultado_df["Estoque"].fillna("Não encontrado saldo", inplace=True)
+    resultado_df["Estoque"].fillna("PEG não encontrado", inplace=True)
     
     return resultado_df
 
